d2e5ec4fbf3d8cb49caca2ef4e2e19fa1b37a3ed
Artur Moreira <arturacm@gmail.com>
2021-12-04 16:44:46 -0300

    add maps project

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,37 @@
+export interface Mapable {
+    location : {
+        lat : number,
+        lng: number
+    },
+    markerContent():string
+}
+
+export class CustomMap{
+    private googleMap: google.maps.Map;
+    constructor(divId: string){
+        this.googleMap = new google.maps.Map(document.getElementById(divId), {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        })
+    }
+    addMarker(mapable: Mapable): void{
+        const marker = new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mapable.location.lat,
+                lng: mapable.location.lng
+            }
+        })
+
+        marker.addListener('click', ()=>{
+            const infoWindow = new google.maps.InfoWindow({
+                content: mapable.markerContent(),
+
+            })
+            infoWindow.open(this.googleMap, marker)
+        })
+    }
+}
