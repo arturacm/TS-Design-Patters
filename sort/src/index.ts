d905fbe69c3ec62c3e7e0013a38f555b87ca1829
Artur Moreira <arturacm@gmail.com>
2021-12-05 16:11:23 -0300

    initial bubble sorting implamentation for array of numbers

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,21 @@
-console.log('hi there', 'hi again', 'and again')
+class Sorter{
+    constructor( public collection: number[]){}
 
-const logger = (text:string) :void=>{
-    console.log(text);
+    sort():void{
+        const { length } = this.collection
+
+        for( let i = 0; i<length; i++){
+            for( let j = 0; j<length-i-1; j++){
+                if ( this.collection[j]>this.collection[j+1]){
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j+1];
+                    this.collection[j+1] = temp;
+                }
+            }
+        }
+    }
 }
 
-logger('aquiii');
+const sorter = new Sorter([1,4,2,-1])
+sorter.sort()
+console.log(sorter.collection)
