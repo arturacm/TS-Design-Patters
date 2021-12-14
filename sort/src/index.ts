c843a0565a2b1121d47685e74d3e5d352702657e
Artur Moreira <arturacm@gmail.com>
2021-12-14 16:28:19 -0300

    refactor the aplication for the abstract classes approach

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -2,11 +2,14 @@ import { CharactersCollection } from './CharactersCollection'
 import { NumbersCollection } from './NumbersCollection'
 import { Sorter } from './Sorter'
 import { LinkedList } from './LinkedList'
+
 // const numbersCollection = new NumbersCollection([1,4,10,-1,5, -10])
+// numbersCollection.sort()
+// console.log(numbersCollection.data)
 
 // const charSorter = new CharactersCollection('asStASx')
-// sorter.sort()
-// console.log(sorter.collection)
+// charSorter.sort()
+// console.log(charSorter.data)
 
 const linkedList = new LinkedList()
 linkedList.add(3)
@@ -15,8 +18,8 @@ linkedList.add(4)
 linkedList.add(100)
 
 linkedList.print()
-const sorter = new Sorter(linkedList)
-sorter.sort()
+
+linkedList.sort()
 
 linkedList.print()
 
